a5f1fd661d51f98564ed75e92593eacc274d987c
blasisd <blasisd@certh.gr>
2023-04-13 12:46:03 +0300

    Fixed ALLOWED_HOSTS default value

--- src/shoppingList/settings.py
+++ src/shoppingList/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRETKEY', 'dummy-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 
 # Application definition
